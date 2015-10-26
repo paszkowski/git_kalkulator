a6fbc3cbb91ca3e4e3ad4cf7aa68f3158f14cc60
Bartosz P <bpaszkowski18@gmail.com>
2015-10-26 11:45:18 +0100

    Added menu based on switch command, and creating objects from user input

--- Calculator/src/figury/Main.java
+++ Calculator/src/figury/Main.java
@@ -7,18 +7,46 @@ public class Main  {
 		
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Podaj bokA prostokatu ");
-		bokA = input.nextInt();
+		System.out.println("Pola do wyboru: ");
+		System.out.println("1 policz pole prostokatu");
+		System.out.println("2 policz pole trojkata");
+		System.out.println("3 policz pole kola");
+		System.out.println(" ");
 		
-		Prostokat stefan = new Prostokat(4, 8);
+		boolean kek = false;
+		int x = input.nextInt();
+		
+		switch(x) {
+		
+		case 1:
+		System.out.println("Podaj bokA i bokB");
+		int A = input.nextInt();
+		int B = input.nextInt();
+		
+		Prostokat stefan = new Prostokat(A, B);
 		stefan.pole();
+		break;
 		
-		Trojkat gloria = new Trojkat(8, 4);
+		case 2:
+			System.out.println("Podaj podstawe i wysokosc");
+			int C = input.nextInt();
+			int D = input.nextInt();
+		Trojkat gloria = new Trojkat(C, D);
 		gloria.pole();
+		break;
 		
-		Kolo bialkov = new Kolo(5);
+		
+		case 3:
+			System.out.println("Podaj promien");
+			int E = input.nextInt();
+		Kolo bialkov = new Kolo(E);
 		bialkov.pole();
-				
+		break;
+		
+		
+		default: System.out.println("Nie wybrano sensownej kategorii");
+		
+		}		
 	}
 
 }
